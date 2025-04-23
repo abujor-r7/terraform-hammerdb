d10a267412c5252b8d9ebb4d70c7b95c8325cc4c
Aleksandrina Bujor <aleksandrina_bujor@rapid7.com>
2025-04-23 18:29:43 +0200

    Add SSH key retrieval via ASM and local storage for Ansible

--- resources/secrets.tf
+++ resources/secrets.tf
@@ -1,3 +1,16 @@
 data "aws_secretsmanager_secret" "aleksa_testbed" {
   name = "aleksa/testbed"
 }
+
+data "aws_secretsmanager_secret_version" "bastion_key" {
+  secret_id = "bastion/ssh-private-key"
+}
+
+resource "null_resource" "write_bastion_key" {
+  provisioner "local-exec" {
+    command = <<EOT
+echo '${data.aws_secretsmanager_secret_version.bastion_key.secret_string}' > ~/.ssh/bastion_key.pem
+chmod 400 ~/.ssh/bastion_key.pem
+EOT
+  }
+}
